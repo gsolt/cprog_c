f0fd9ee006db2d589842acc2e88598b238d363d1
Gergely Zsolt <gzsolt0@gmail.com>
2022-12-15 15:11:07 +0100

    80-49 LINE8

--- PARS_C.c
+++ PARS_C.c
@@ -253,7 +253,7 @@ ST[0].nSiteList[56]		=5063; 	    ST[0].nLinkList[56]   	=RADIO9_4;	/* RADIO1_1 T
 ST[0].nSiteList[57]		=5035; 	    ST[0].nLinkList[57]   	=RADIO6_1;	/* RADIO6_1 TMOK, 40-98 		SZOM*/
 ST[0].nSiteList[58]		=5065; 	    ST[0].nLinkList[58]   	=LINE6;	    /* RADIO1_8 TMOK, 35-11 		SZVAR*/
 ST[0].nSiteList[59]		=5040; 	    ST[0].nLinkList[59]   	=LINE17;	  /* RADIO4_2 TMOK, 70-29 		IK*/
-ST[0].nSiteList[60]		=5050; 	    ST[0].nLinkList[60]   	=RADIO5_2;	/* RADIO3_6 TMOK, 80-49 		OR*/
+ST[0].nSiteList[60]		=5050; 	    ST[0].nLinkList[60]   	=LINE8;	    /* RADIO3_6 TMOK, 80-49 		OR*/
 ST[0].nSiteList[61]		=5091; 	    ST[0].nLinkList[61]   	=RADIO4_2;	/* RADIO4_2 TMOK, 72-33 		IK*/
 ST[0].nSiteList[62]		=5093; 	    ST[0].nLinkList[62]   	=RADIO4_2;	/* RADIO4_2 TMOK, 71-39 		IK*/
 ST[0].nSiteList[63]		=5062; 	    ST[0].nLinkList[63]   	=RADIO9_1;	/* RADIO1_1 TMOK, 32-88 		SZVAR*/
