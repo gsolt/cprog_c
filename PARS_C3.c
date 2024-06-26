ceadd1b7b18bdf54fac8e7bcbbfad979b3455ef6
Gergely Zsolt <gzsolt0@gmail.com>
2024-06-26 16:02:35 +0200

    92-51 PM6

--- PARS_C3.c
+++ PARS_C3.c
@@ -2294,7 +2294,11 @@ sT[0].sTI[186].nType = TYP_TMOK;
 
 	sT[0].sMOT[186].nIEC_SP				= 3414;		/* Egybites értékek IEC kezdõcíme*/
 	sT[0].sMOT[186].nIEC_DP				= 441;		/* Kétbites értékek IEC kezdõcíme*/
-	sT[0].sMOT[186].nIEC_NM				= 597;		/* Mérések IEC kezdõcíme */
+	sT[0].sMOT[186].nIEC_NM				= 1783;		/* Mérések IEC kezdõcíme */
+
+  sT[0].sMOT[186].nNMNum     			= 4;		  /* Mérések száma */
+	sT[0].sMOT[186].nIEC_NM2				= 1787;		/* Mérések IEC kezdõcíme */	
+	sT[0].sMOT[186].nNMNum2				  = 2;		  /* Analóg mérések száma*/
 
 /*  86-63 OK2  -----------------------------------------------------------*/
 sT[0].sTI[187].nType = TYP_TMOK;
